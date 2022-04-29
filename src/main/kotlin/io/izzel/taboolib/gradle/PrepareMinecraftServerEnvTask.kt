3e5f719b9e4bf418532712d1483bd1bed62a48ec
方糖 <cubesugarcheese@qq.com>
2022-04-29 17:06:29 +0800

    Create PrepareMinecraftServerEnvTask.kt
    添加部署服务器环境任务

---
+++ src/main/kotlin/io/izzel/taboolib/gradle/PrepareMinecraftServerEnvTask.kt
@@ -0,0 +1,59 @@
+import org.gradle.api.DefaultTask
+
+abstract class PrepareMinecraftServerEnvTask: DefaultTask() {
+    init { group="TabooLibLoom" }
+    @get:Input
+    @get:Option(option = "jarUrl", description = "To download the server jar.")
+    abstract val jarUrl: Property<String>
+
+    @get:Input
+    @get:Optional
+    @get:Option(option = "jarName", description = "The name of the downloaded Jar file")
+    abstract val jarName: Property<String>
+
+    private val jarNameOrDefault
+        get() = jarName.orElse("server.jar")
+
+    @get:Input
+    @get:Optional
+    @get:Option(option = "serverDirectory", description = "For storing server data.")
+    abstract val serverDirectory: DirectoryProperty
+
+    private val serverDirectoryOrDefault
+        get() = serverDirectory.orElse(project.layout.buildDirectory.dir("MinecraftServer"))
+
+    @get:Input
+    @get:Optional
+    @get:Option(option = "agreeEula", description = "Agree to the Minecraft EULA")
+    abstract val agreeEula: Property<Boolean>
+
+    private val agreeEulaOrDefault
+        get() = agreeEula.orElse(false)
+
+    @TaskAction
+    fun launchServer() {
+        val jarUrl = jarUrl.get()
+        val serverDirectory = serverDirectoryOrDefault.get().asFile.apply(File::mkdirs)
+        val jarFile = serverDirectory.resolve(jarNameOrDefault.get())
+        if (jarFile.exists().not()) {
+            logger.lifecycle(
+                """
+                    Downloading Jar
+                        url : $jarUrl
+                        dest : ${jarFile.absolutePath}
+                """.trimIndent()
+            )
+            downloadFile(jarUrl, jarFile)
+        }
+        if (agreeEulaOrDefault.get()) {
+            val eulaFile = serverDirectory.resolve("eula.txt")
+            if (eulaFile.exists().not() || eulaFile.readText().contains("eula=true").not()) {
+                eulaFile.writeText("eula=true")
+            }
+        }
+    }
+
+    private fun downloadFile(url: String, dest: File) {
+        ant.invokeMethod("get", mapOf("src" to url, "dest" to dest))
+    }
+}
